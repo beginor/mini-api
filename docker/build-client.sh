295acbe72375f8f5b8f1ec1938715bea091200de
beginor <beginor@qq.com>
2025-08-18 16:03:56 +0800

    add build-client scripts

---
+++ docker/build-client.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+# Build client project;
+cd ../../mini-api-client
+rm -rf dist
+pnpm build app-shared
+pnpm build web
+# pnpm build handset
+cd ../mini-api/docker
+# Check wwwroot folder
+if [ -d dist/wwwroot ]
+then
+  rm -rf mkdir -p dist/wwwroot
+fi
+mkdir -p dist/wwwroot
+# Copy client files to wwwroot
+cp -r ../../mini-api-client/dist/web/browser dist/wwwroot/web
+# cp -r ../../mini-api-client/dist/handset/browser dist/wwwroot/handset
+# Gzip static files (*.js, *.css).
+find dist -name "*.js" -print0 | xargs -0 gzip -k
+find dist -name "*.css" -print0 | xargs -0 gzip -k
